813af9d158779337cf852960737bb5b91800f178
wujianming <wujianming@wtoip.com>
2018-07-02 19:50:03 +0800

    测试动态请求

--- 1/sw.js
+++ 1/sw.js
@@ -4,6 +4,8 @@ var cacheName = 'latestNews-v1';
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
+
+    //外壳
     .then(cache => cache.addAll([
       './main.js',
       './huoying.jpg',
@@ -13,31 +15,31 @@ self.addEventListener('install', event => {
   );
 });
 
-// Cache any new resources as they are fetched
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request, { ignoreSearch: true })
-//     .then(function(response) {
-//       if (response) {
-//         return response;
-//       }
-//       var fetchRequest = event.request.clone();
-//
-//       return fetch(fetchRequest).then(
-//         function(response) {
-//           if(!response || response.status !== 200) {
-//             return response;
-//           }
-//
-//           var responseToCache = response.clone();
-//           caches.open(cacheName)
-//           .then(function(cache) {
-//             cache.put(event.request, responseToCache);
-//           });
-//
-//           return response;
-//         }
-//       );
-//     })
-//   );
-// });
+// 动态请求
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true })
+    .then(function(response) {
+      if (response) {
+        return response;
+      }
+      var fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then(
+        function(response) {
+          if(!response || response.status !== 200) {
+            return response;
+          }
+
+          var responseToCache = response.clone();
+          caches.open(cacheName)
+          .then(function(cache) {
+            cache.put(event.request, responseToCache);
+          });
+
+          return response;
+        }
+      );
+    })
+  );
+});
