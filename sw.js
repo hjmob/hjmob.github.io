84ceea5a5164137810a273bb9af319bdcee1880c
wujianming <wujianming@wtoip.com>
2018-07-25 16:24:45 +0800

    测试pwa性能优化

--- sw.js
+++ sw.js
@@ -1,50 +1,111 @@
-var cacheName = 'v2';
+let cacheName = 'cache-version-187';
+let isSaveHtml = false;//是否保存html
 
-// 安装阶段跳过等待，直接进入 active
+//用于判断用户第一次注册service worker,不提示更新
+let isFirstServiceWorker = true;
+let version = 37;
+
+let cacheSource = [
+  `/test.js?v=${version}`,
+  `/huoying.jpg?v=${version}`,
+  `/style.css?v=${version}`
+]
+
+//安装阶段跳过等待，直接进入 active
 self.addEventListener('install', function(event) {
-    event.waitUntil(self.skipWaiting());
+      let p = Promise.all([// 清理旧版本
+        caches.keys().then(function (cacheList) {
+            return Promise.all(
+                cacheList.map(function (cName) {
+                    isFirstServiceWorker = false;
+                    if (cName !== cacheName) {
+                        return caches.delete(cName);
+                    }
+                })
+            );
+        }),
+        caches.open(cacheName).then(cache => cache.addAll(cacheSource))
+    ]);
+    p.then(()=>{
+      self.skipWaiting()
+    })
+
+    event.waitUntil(p);
 });
 
-self.addEventListener('activate', event => event.waitUntil(
-    Promise.all([
-        // 更新客户端
-        clients.claim(),
-        // 清理旧版本
-        caches.keys().then(cacheList => Promise.all(
-            cacheList.map(cName => {
-                if (cName !== cacheName) {
-                    caches.delete(cacheName);
-                }
-            })
-        ))
-    ])
-));
+self.addEventListener('activate', event => {
+  let p = self.clients.claim();
+  if( ! isFirstServiceWorker ){
+    p.then(()=>{
+      //向主进程发送消息，提醒用户更新
+      self.clients.matchAll().then(clientList => {
+          clientList.forEach(client => {
+              //client.postMessage('reload');
+          })
+      });
+    })
+  }
 
-// 动态请求
+  event.waitUntil(p);
+
+});
+
+//监听fetch请求事件，除 sw.js 请求不监听外，其他资源都会被监听
 self.addEventListener('fetch', function(event) {
+  //可以看到哪些东西会触发fetch事件
+  console.log("请求地址:",event.request.url)
+
   event.respondWith(
-    caches.match(event.request, { ignoreSearch: true })
+    caches.match(event.request)
     .then(function(response) {
       if (response) {
         return response;
       }
+
       var fetchRequest = event.request.clone();
 
       return fetch(fetchRequest).then(
-        function(response) {
-          if(!response || response.status !== 200) {
-            return response;
+        function(fetchResponse) {
+          if(!fetchResponse || fetchResponse.status !== 200) {
+            return fetchResponse;
           }
 
-          var responseToCache = response.clone();
-          caches.open(cacheName)
-          .then(function(cache) {
-            cache.put(event.request, responseToCache);
-          });
+          var responseToCache = fetchResponse.clone();
 
-          return response;
+          if( isSaveHtml ){
+            caches.open(cacheName)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+          }else{//不保存html
+            if( checkFileExt( responseToCache.url ) ){
+              caches.open(cacheName)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+            }
+          }
+
+          return fetchResponse;
         }
       );
     })
   );
 });
+
+//检测文件后缀名是否在白名单中
+function checkFileExt(filename){
+   var flag = false; //状态
+   var arr = ["jpg","png","gif",'css','js',"webp","woff"];
+   //取出上传文件的扩展名
+   var index = filename.lastIndexOf(".");
+   var ext = filename.substr( index + 1 );
+
+   for( var i = 0; i < arr.length; i++ ){
+     if( ext.indexOf(arr[i]) > 0 ){
+      flag = true;
+      break;
+     }
+   }
+   return flag;
+}
